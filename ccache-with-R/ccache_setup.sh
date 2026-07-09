49637580fb79a7309436dd6ed76bb7422531ab8d
Dirk Eddelbuettel <edd@debian.org>
2026-07-09 08:29:02 -0500

    Add mtime to sloppiness

--- ccache-with-R/ccache_setup.sh
+++ ccache-with-R/ccache_setup.sh
@@ -39,7 +39,7 @@ EOF
 # set base and cache directories, helps against random paths
 ccache --set-config "base_dir=$(pwd)"
 ccache --set-config "cache_dir=$(pwd)/.ccache"
-# important for R CMD INSTALL *.tar.gz as tarballs are expanded freshly -> fresh ctime
-ccache --set-config "sloppiness=include_file_ctime"
+# important for R CMD INSTALL *.tar.gz as tarballs are expanded freshly -> fresh ctime, also mtime
+ccache --set-config "sloppiness=include_file_ctime,include_file_mtime"
 # also important as the (temporary) directory name will differ
 ccache --set-config "hash_dir=false"
